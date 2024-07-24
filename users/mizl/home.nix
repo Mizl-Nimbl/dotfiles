008c8657d1990d8191db48d49e0b5e9acb72ea6b
Mizl Nimbl <nimbl@mizl.dev>
2024-07-23 20:40:53 -0500

    Change

--- users/mizl/home.nix
+++ users/mizl/home.nix
@@ -34,6 +34,9 @@
     git-crypt
     gnupg
     pinentry-curses
+    kdenlive
+    krita
+    obsidian
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
