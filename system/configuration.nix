ef57095374ff3a029be3fc01132b785765968753
Mizl Nimbl <nimbl@mizl.dev>
2024-07-23 22:50:41 -0500

    Change

--- system/configuration.nix
+++ system/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-
+  hardware.opengl.setLdLibraryPath = true;
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
