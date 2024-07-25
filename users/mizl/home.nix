5580e7fdd716bac888740efe5703c00a9f09878d
Mizl Nimbl <nimbl@mizl.dev>
2024-07-25 18:47:58 -0500

    Change

--- users/mizl/home.nix
+++ users/mizl/home.nix
@@ -51,6 +51,7 @@
     capitaine-cursors-themed
     nmap
     vlc
+    mullvad-vpn
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
