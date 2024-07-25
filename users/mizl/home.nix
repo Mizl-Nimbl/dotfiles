f49b8cf55c4978fbec63fdbcba358a52fe4c130d
Mizl Nimbl <nimbl@mizl.dev>
2024-07-25 12:50:26 -0500

    Change

--- users/mizl/home.nix
+++ users/mizl/home.nix
@@ -50,6 +50,7 @@
     gruvbox-plus-icons
     capitaine-cursors-themed
     nmap
+    vlc
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
