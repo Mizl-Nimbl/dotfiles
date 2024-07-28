70dd9c9b00cdc8fc8e98c4c40f723ed8d7502a96
Mizl Nimbl <nimbl@mizl.dev>
2024-07-28 13:25:19 -0500

    Change

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
-{ self, ... }@inputs:
+{ self, nixpkgs, ... }@inputs:
 {
   nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [ ./system/shared.nix ./system/desktop.nix ];
   };
-  
+
   nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [ ./system/shared.nix ./system/laptop.nix ];
