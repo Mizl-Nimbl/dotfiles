5a1e5895d20ad894d96c66a50855fcbcc2d6a493
Mizl Nimbl <nimbl@mizl.dev>
2024-07-28 13:32:31 -0500

    Change

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
-{ self, nixpkgs, ... }@inputs:
 {
-  nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [ ./system/shared.nix ./system/desktop.nix ];
-  };
+  description = "mizl nixOS";
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./system/shared.nix ./system/desktop.nix ];
+      };
 
-  nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [ ./system/shared.nix ./system/laptop.nix ];
-  };
-}
+      nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./system/shared.nix ./system/laptop.nix ];
+      };
+    }
+};
