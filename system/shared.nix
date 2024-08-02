9c5a67ab785509034740052a4eb0afa1903d6810
Mizl-Nimbl <nimbl@mizl.dev>
2024-08-02 00:35:34 -0500

    Change

--- system/shared.nix
+++ system/shared.nix
@@ -63,12 +63,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+
+  users.extraUsers.mizl.extraGroups = [ "jackaudio" ];
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
